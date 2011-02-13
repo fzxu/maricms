7b4014cffebf9715a195f012488748bad98bc9b2
arkxu <ark.work@gmail.com>
2011-02-13 14:58:55 +0800

    mtheme

--- app/controllers/mthemes_controller.rb
+++ app/controllers/mthemes_controller.rb
@@ -6,11 +6,12 @@ class MthemesController < ApplicationController
     if Dir.exist?(File.join(Rails.root, "themes", theme_name))
       FileUtils.remove_dir(File.join(Rails.root, "themes", theme_name))
     end
-    notice = `rails g theme_for_tt:theme #{theme_name}`
     `svnadmin create /home/svn/#{theme_name}`
     `chown svn /home/svn/#{theme_name}`
     `chgrp www-data /home/svn/#{theme_name}`
     `svn co svn+ssh://#{@setting.host_name}/home/svn/#{theme_name}`
+
+    notice = `rails g theme_for_tt:theme #{theme_name}`
     
     respond_to do |format|
       format.html { redirect_to '/setting', :notice => notice}

e4d278ee784a34bb8c3f4ae2d377d09249f83933
arkxu <ark.work@gmail.com>
2011-02-13 15:26:30 +0800

    change svn command

--- app/controllers/mthemes_controller.rb
+++ app/controllers/mthemes_controller.rb
@@ -6,12 +6,20 @@ class MthemesController < ApplicationController
     if Dir.exist?(File.join(Rails.root, "themes", theme_name))
       FileUtils.remove_dir(File.join(Rails.root, "themes", theme_name))
     end
+    if Dir.exist?("/home/svn/default")
+      FileUtils.remove_dir("/home/svn/default")
+    end
+
     `svnadmin create /home/svn/#{theme_name}`
-    `chown svn /home/svn/#{theme_name}`
-    `chgrp www-data /home/svn/#{theme_name}`
-    `svn co svn+ssh://#{@setting.host_name}/home/svn/#{theme_name}`
+    `chown -R svn /home/svn/#{theme_name}`
+    `chgrp -R www-data /home/svn/#{theme_name}`
+    `svn co svn+ssh://svn@#{@setting.host_name}/home/svn/#{theme_name} #{File.join(Rails.root, "themes", theme_name)}`
 
     notice = `rails g theme_for_tt:theme #{theme_name}`
+    
+    `cd #{File.join(Rails.root, "themes", theme_name)}`
+    `svn add *`
+    `svn commit -m "init"`
     
     respond_to do |format|
       format.html { redirect_to '/setting', :notice => notice}

cdec6361e608f5e43f1b84fe687ae118ebc48afd
arkxu <ark.work@gmail.com>
2011-03-06 13:40:56 +0800

    check tab exist before rendering template

--- app/controllers/tabs_controller.rb
+++ app/controllers/tabs_controller.rb
@@ -5,7 +5,7 @@ class TabsController < ApplicationController
   # GET /tabs.xml
   def index
     @d = D.find(params[:d])
-  
+
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @tabs }
@@ -17,87 +17,91 @@ class TabsController < ApplicationController
   def show
     begin
       @tab = nil
-      
+
       if params[:id]
-      # loop all the tab datasource and find the first one has the specific slug
+        # loop all the tab datasource and find the first one has the specific slug
         tab_ds = D.where(:ds_type => "Tab")
         tab_ds.each do |d|
           @tab = d.get_klass.find(:first, :conditions => {:slug => params[:id]}) || d.get_klass.find(params[:id])
           if @tab
-            break
+          break
           end
         end
       end
-      
+
       # get the first tab datasource's root
       unless @tab
         @tab = D.where(:ds_type => "Tab").first.get_klass.root
       end
 
-      @page = @tab.page
+      if @tab
+        @page = @tab.page
 
-      if @page
-        begin
-          template_content = IO.read(File.join(theme_path, "theme", @page.theme_path ))
-        rescue
-          template_content = IO.read(File.join(theme_path, "theme", "page_default.html" ))
-        end
-        template = Liquid::Template.parse(template_content)  # Parses and compiles the template
-        #TODO need to cache the template somewhere in future
+        if @page
+          begin
+            template_content = IO.read(File.join(theme_path, "theme", @page.theme_path ))
+          rescue
+            template_content = IO.read(File.join(theme_path, "theme", "page_default.html" ))
+          end
+          template = Liquid::Template.parse(template_content)  # Parses and compiles the template
+          #TODO need to cache the template somewhere in future
 
-        #Assemble the variable and it's content, and then pass to template
-        render_params = Hash.new
+          #Assemble the variable and it's content, and then pass to template
+          render_params = Hash.new
 
-        #add the parameters to the template
-        param_hash = params
-        # unless @tab.param_string.blank?
-        #   @tab.param_string.split('&').each do |p_str|
-        #     pair = p_str.strip.split('=')
-        #     param_hash[pair.first] = pair.last
-        #   end
-        # end
-        render_params["params"] = param_hash
-        #add the tabs to the template
-        # tabs = Array.new
-        # Tab.traverse(:depth_first) do |tab|
-        #   tabs << tab
-        # end
-        # render_params["tabs"] = tabs
-        render_params["current_tab"] = @tab
+          #add the parameters to the template
+          param_hash = params
+          # unless @tab.param_string.blank?
+          #   @tab.param_string.split('&').each do |p_str|
+          #     pair = p_str.strip.split('=')
+          #     param_hash[pair.first] = pair.last
+          #   end
+          # end
+          render_params["params"] = param_hash
+          #add the tabs to the template
+          # tabs = Array.new
+          # Tab.traverse(:depth_first) do |tab|
+          #   tabs << tab
+          # end
+          # render_params["tabs"] = tabs
+          render_params["current_tab"] = @tab
 
-        # Query the datasource based on the parameters
-        q = {}
-        if param_hash
-          param_hash.each do |k,v|
-            s = k.split(".")
-            if s && s.size > 2 && s[0] == "ds"
-              q[s[1]] = {s[2] => v}
+          # Query the datasource based on the parameters
+          q = {}
+          if param_hash
+            param_hash.each do |k,v|
+              s = k.split(".")
+              if s && s.size > 2 && s[0] == "ds"
+                q[s[1]] = {s[2] => v}
+              end
             end
           end
-        end
 
-        r_page_ds = @page.r_page_ds
-        if r_page_ds && r_page_ds.size > 0
-          for r_page_d in r_page_ds
-            d_key = r_page_d.d.key
-            unless r_page_d.new_d_name.blank?
+          r_page_ds = @page.r_page_ds
+          if r_page_ds && r_page_ds.size > 0
+            for r_page_d in r_page_ds
+              d_key = r_page_d.d.key
+              unless r_page_d.new_d_name.blank?
               d_key = r_page_d.new_d_name
-            end
-            if q[d_key].nil?
-            render_params[d_key] = r_page_d.default_query.paginate(:page => params[:page], :per_page => @page.per_page || 20)
-            else
-            render_params[d_key] = r_page_d.default_query.where(q[d_key]).paginate(:page => params[:page], :per_page => @page.per_page || 20)
-            end
-  
-          end
-        end
+              end
+              if q[d_key].nil?
+                render_params[d_key] = r_page_d.default_query.paginate(:page => params[:page], :per_page => @page.per_page || 20)
+              else
+                render_params[d_key] = r_page_d.default_query.where(q[d_key]).paginate(:page => params[:page], :per_page => @page.per_page || 20)
+              end
 
-        respond_to do |format|
-          format.html { render :layout => "front", :text => template.render(render_params, :registers => {:controller => self})}
-          format.xml  { render :xml => @page }
+            end
+          end
+
+          respond_to do |format|
+            format.html { render :layout => "front", :text => template.render(render_params, :registers => {:controller => self})}
+            format.xml  { render :xml => @page }
+          end
+        else
+          render :text => "no page binds to this #{@tab.slug} tab!"
         end
       else
-        render :text => "no page binds to this #{@tab.slug} tab!"
+        render :text => "no tab find!"
       end
     rescue BSON::InvalidObjectId => e
       render :text => "page not found" + e.to_s
@@ -109,38 +113,38 @@ class TabsController < ApplicationController
   def new
     @d = D.find(params[:d])
     @tab = @d.get_klass.new
-  
+
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @tab }
     end
   end
-  
+
   # GET /tabs/1/edit
   def edit
     #@tab = Tab.find(:first, :conditions => {:slug => params[:id]}) || Tab.find(params[:id])
     @d = D.find(params[:d])
     @tab = @d.get_klass.find(params[:id])
   end
-  
+
   # POST /tabs
   # POST /tabs.xml
   def create
     @d = D.find(params[:d])
     parent_id = params[:tab].delete(:parent_id)
     page_id = params[:tab].delete(:page_id)
-  
+
     @tab = @d.get_klass.new(params[:tab])
-  
+
     if page_id && !page_id.blank?
     @page = Page.find(page_id)
     @tab.page = @page
     end
-  
+
     unless parent_id.blank?
     @tab.parent = @d.get_klass.find(parent_id)
     end
-  
+
     respond_to do |format|
       if @tab.save
         #@tab.move_to_bottom
@@ -152,7 +156,7 @@ class TabsController < ApplicationController
       end
     end
   end
-  
+
   # PUT /tabs/1
   # PUT /tabs/1.xml
   def update
@@ -162,7 +166,7 @@ class TabsController < ApplicationController
     @tab = @d.get_klass.find(params[:id])
     unless page_id.blank?
     @page = Page.find(page_id)
-  
+
     # should be mongoid bug, remove the old page's tab id
     #      if @tab.page
     # old_page_id = @tab.page.id
@@ -176,7 +180,7 @@ class TabsController < ApplicationController
     unless parent_id.blank?
     @tab.parent = @d.get_klass.find(parent_id)
     end
-  
+
     respond_to do |format|
       if @tab.update_attributes(params[:tab]) && @tab.save
         format.html { redirect_to(tabs_path(:d => @d.id), :notice => 'Tab was successfully updated.') }
@@ -187,37 +191,37 @@ class TabsController < ApplicationController
       end
     end
   end
-  
+
   # DELETE /tabs/1
   # DELETE /tabs/1.xml
   def destroy
     @d = D.find(params[:d])
     @tab = @d.get_klass.find(params[:id])
     @tab.destroy
-  
+
     respond_to do |format|
       format.html { redirect_to(tabs_path(:d => @d.id)) }
       format.xml  { head :ok }
     end
   end
-  
+
   def move_up
     @d = D.find(params[:d])
     @tab = @d.get_klass.find(params[:id])
     @tab.move_up
-  
+
     respond_to do |format|
       format.html {redirect_to(tabs_path(:d => @d.id))}
       format.xml  { head :ok }
     end
   end
-  
+
   def move_down
     @d = D.find(params[:d])
     @tab = @d.get_klass.find(params[:id])
 
     @tab.move_down
-  
+
     respond_to do |format|
       format.html { redirect_to(tabs_path(:d => @d.id))}
       format.xml  { head :ok }

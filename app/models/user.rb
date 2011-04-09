6dbdb264f9553023515a26c48972c6ee291aa017
arkxu <ark.work@gmail.com>
2011-04-10 01:30:32 +0800

    switch off user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User
   include Mongoid::Document
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, #:registerable,
     :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable, :token_authenticatable ,:timeoutable
 
   # Virtual attribute for authenticating by either username or email

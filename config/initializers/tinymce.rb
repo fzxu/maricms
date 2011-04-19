d602862ac69e9e2a3c5df18d210b74303463e1a1
arkxu <ark.work@gmail.com>
2011-04-20 00:45:53 +0800

    replace kindeditor with tinymce

---
+++ config/initializers/tinymce.rb
@@ -0,0 +1,26 @@
+TINYMCE_SETTING = <<-S
+
+// Location of TinyMCE script
+      script_url : '/javascripts/tiny_mce/tiny_mce.js',
+
+      // General options
+      theme : "advanced",
+      skin: "default",
+      plugins : "autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,advlist",
+
+      // Theme options
+      theme_advanced_buttons1 : "code,|,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,formatselect,fontselect,fontsizeselect",
+      theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+      theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,fullscreen",
+      theme_advanced_buttons4 : "styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking",
+      theme_advanced_toolbar_location : "top",
+      theme_advanced_toolbar_align : "left",
+      theme_advanced_statusbar_location : "bottom",
+      theme_advanced_resizing : true,
+
+      // Example content CSS (should be your site CSS)
+      content_css : "/javascripts/tiny_mce/themes/advanced/skins/default/content.css",
+      
+      width : "90%",
+      height: "250"
+S

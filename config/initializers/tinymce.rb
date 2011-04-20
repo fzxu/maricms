805d11f3ec0013523d2dc444f043fa3e83fbbbf5
arkxu <ark.work@gmail.com>
2011-04-20 18:06:43 +0800

    add wp_theme and adv button for editor

--- config/initializers/tinymce.rb
+++ config/initializers/tinymce.rb
@@ -5,11 +5,11 @@ TINYMCE_SETTING = <<-S
 
       // General options
       theme : "advanced",
-      skin: "default",
-      plugins : "autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,advlist",
+      skin: "wp_theme",
+      plugins : "autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,advlist,wordpress",
 
       // Theme options
-      theme_advanced_buttons1 : "code,|,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,formatselect,fontselect,fontsizeselect",
+      theme_advanced_buttons1 : "code,|,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,formatselect,fontselect,fontsizeselect,wp_adv",
       theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,|,insertdate,inserttime,preview,|,forecolor,backcolor",
       theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,fullscreen",
       //theme_advanced_buttons4 : "styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking",
@@ -21,7 +21,7 @@ TINYMCE_SETTING = <<-S
       // Example content CSS (should be your site CSS)
       content_css : "/javascripts/tiny_mce/themes/advanced/skins/default/content.css",
       
-      width : "90%",
+      width : "100%",
       height: "250",
       language: "zh-cn"
 S

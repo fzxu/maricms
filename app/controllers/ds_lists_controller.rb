1b674be63a9fa8719f139c94a4dd26185df3f6cf
arkxu <ark.work@gmail.com>
2011-06-06 23:05:01 +0800

    n 2 n with ui

--- app/controllers/ds_lists_controller.rb
+++ app/controllers/ds_lists_controller.rb
@@ -8,36 +8,36 @@
 # MariGold MariCMS is free software: you can redistribute it and/or
 # modify it under the terms of the GNU Affero General Public License as published
 # by the Free Software Foundation, version 3 of the License.
-# 
+#
 # Under the terms of the GNU Affero General Public License you must release the
 # complete source code for any application that uses any part of MariCMS
 # (system header files and libraries used by the operating system are excluded).
 # These terms must be included in any work that has MariCMS components.
 # If you are developing and distributing open source applications under the
 # GNU Affero General Public License, then you are free to use MariCMS.
-# 
+#
 # If you are deploying a web site in which users interact with any portion of
 # MariCMS over a network, the complete source code changes must be made
 # available.  For example, include a link to the source archive directly from
 # your web site.
-# 
+#
 # For OEMs, ISVs, SIs and VARs who distribute MariCMS with their products,
 # and do not license and distribute their source code under the GNU
 # Affero General Public License, MariGold provides a flexible commercial
 # license.
-# 
+#
 # To anyone in doubt, we recommend the commercial license. Our commercial license
 # is competitively priced and will eliminate any confusion about how
 # MariCMS can be used and distributed.
-# 
+#
 # MariCMS is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
 # details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with MariCMS.  If not, see <http://www.gnu.org/licenses/>.
-# 
+#
 # Attribution Notice: MariCMS is an Original Work of software created
 # by  金盏信息科技(上海)有限公司 | MariGold Information Tech. Co,. Ltd.
 ##
@@ -45,12 +45,15 @@
 class DsListsController < ApplicationController
   before_filter :get_setting
   before_filter :authenticate_user!
-  
   def index
     @d = D.find(params[:d])
 
     respond_to do |format|
       format.html {render :layout => "ds_view_#{@d.ds_view_type.downcase}" }
+      format.js {
+        @disp_field =  params[:disp_field]
+        render "index_js", :layout => false, :content_type => "text/html"
+      }
       format.xml  { render :xml => @tabs }
     end
   end
@@ -78,13 +81,13 @@ class DsListsController < ApplicationController
 
   def create
     mg_url = params[:record].delete(:mg_url)
-    
+
     @d = D.find(params[:d])
     @record = @d.get_klass.new(params[:record])
 
     # update the mg url
     unless mg_url[:path].blank?
-      @record.mg_url = MgUrl.new(mg_url)
+    @record.mg_url = MgUrl.new(mg_url)
     end
 
     # process the relationship
@@ -96,6 +99,14 @@ class DsListsController < ApplicationController
             related_record = D.where(:key => ds_element.relation_ds).first.get_klass.find(p_related_record)
             @record.send("#{ds_element.key}=", related_record)
           end
+        elsif ds_element.relation_type == "has_many"
+          p_related_records = params[:record].delete("#{ds_element.key}")
+          if p_related_records && !p_related_records.empty?
+            p_related_records.each do |p_related_record|
+              related_record = D.where(:key => ds_element.relation_ds).first.get_klass.find(p_related_record)
+              @record.send("#{ds_element.key}") << related_record
+            end
+          end          
         end
       end
     end
@@ -105,7 +116,7 @@ class DsListsController < ApplicationController
         expire_action_cache(@record)
         format.html { redirect_to(ds_lists_path(:d => @d.id))}
         format.js {
-          render "shared/close_dialog" 
+          render "shared/close_dialog"
         }
         format.xml { head :ok}
       else
@@ -114,11 +125,11 @@ class DsListsController < ApplicationController
       end
     end
   end
-  
+
   def edit
     @d = D.find(params[:d])
     @record = @d.get_klass.find(params[:id])
-    
+
     respond_to do |format|
       format.html {render :layout => "ds_view_#{@d.ds_view_type.downcase}" }
       format.xml  { render :xml => @record }
@@ -127,22 +138,22 @@ class DsListsController < ApplicationController
 
   def update
     mg_url = params[:record].delete(:mg_url)
-    
+
     @d = D.find(params[:d])
     @record = @d.get_klass.find(params[:id])
 
     if mg_url[:path].blank?
       if @record.mg_url
-        @record.mg_url.destroy
+      @record.mg_url.destroy
       end
     else
       if @record.mg_url
-        @record.mg_url.update_attributes(mg_url)
+      @record.mg_url.update_attributes(mg_url)
       else
-        @record.mg_url = MgUrl.new(mg_url)
+      @record.mg_url = MgUrl.new(mg_url)
       end
     end
-    
+
     # process the relationship
     @d.ds_elements.each do |ds_element|
       if ds_element.ftype == "Relation"
@@ -152,10 +163,18 @@ class DsListsController < ApplicationController
             related_record = D.where(:key => ds_element.relation_ds).first.get_klass.find(p_related_record)
             @record.send("#{ds_element.key}=", related_record)
           end
+        elsif ds_element.relation_type == "has_many"
+          p_related_records = params[:record].delete("#{ds_element.key}")
+          if p_related_records && !p_related_records.empty?
+            p_related_records.each do |p_related_record|
+              related_record = D.where(:key => ds_element.relation_ds).first.get_klass.find(p_related_record)
+              @record.send("#{ds_element.key}") << related_record
+            end
+          end          
         end
       end
     end
-    
+
     respond_to do |format|
       if @record.update_attributes(params[:record])
         expire_action_cache(@record)
@@ -168,16 +187,14 @@ class DsListsController < ApplicationController
     end
   end
 
-
   def destroy
     @d = D.find(params[:d])
     @record = @d.get_klass.find(params[:id])
     @mg_url = @record.mg_url
     @record.destroy
     if @mg_url
-      @mg_url.destroy
+    @mg_url.destroy
     end
-
 
     respond_to do |format|
       expire_action_cache(@record)
@@ -223,12 +240,12 @@ class DsListsController < ApplicationController
   end
 
   private
-  
+
   def current_records(d, params={})
     current_page = (params[:iDisplayStart].to_i/params[:iDisplayLength].to_i rescue 0)+1
 
     if params[:sSearch].blank?
-      result = d.get_klass.all
+    result = d.get_klass.all
     else
       result = d.get_klass.any_of(conditions(d, params))
     end
@@ -237,7 +254,7 @@ class DsListsController < ApplicationController
     result.desc(:position).paginate :page => current_page,
     :per_page => params[:iDisplayLength]
   end
-  
+
   def total_records(d)
     d.get_klass.all.size
   end
@@ -249,7 +266,7 @@ class DsListsController < ApplicationController
       if  field.last.type == "Integer" && sSearch.to_i.to_s == sSearch
         cond << {"#{field.last.name}".to_sym => sSearch.to_i}
       elsif
-        cond << {"#{field.last.name}".to_sym => /#{sSearch}/}
+      cond << {"#{field.last.name}".to_sym => /#{sSearch}/}
       end
     end
     return cond

5e81492b25e060a0c1b79d464ae6388dccb2a632
arkxu <ark.work@gmail.com>
2011-06-16 21:07:38 +0800

    specify the yaml engine for ruby 1.9.*

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
-# require 'yaml' 
-# YAML::ENGINE.yamler= 'syck'
+if RUBY_VERSION =~ /1\.9/
+  require 'yaml'
+  YAML::ENGINE.yamler= 'syck'
+end
 
 # Load the rails application
 require File.expand_path('../application', __FILE__)
